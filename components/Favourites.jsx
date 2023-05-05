a41db4cc696b70b732efdc37f7bb4784c6260f64
samarth-shinde <samarthshinde247@gmail.com>
2023-05-05 16:17:44 +0530

    finished favourites and fixed playing bugs

--- components/Favourites.jsx
+++ components/Favourites.jsx
@@ -2,6 +2,7 @@ import { useSession } from "next-auth/react";
 import React, { useState, useEffect } from "react";
 import Song from "./Song";
 import Profile from "./Profile";
+import { HeartIcon } from "@heroicons/react/24/solid";
 export default function Favourites({
   setView,
   setGlobalArtistId,
@@ -10,25 +11,6 @@ export default function Favourites({
 }) {
   const { data: session } = useSession();
   const [favourites, setFavourites] = useState([]);
-  //   const [opacity, setOpacity] = useState(0);
-  //   const [textOpacity, setTextOpacity] = useState(0);
-  //   function changeOpacity(scrollPos) {
-  //     // scrollPos = 0 -> opacity = 0
-  //     // scrollPos = 300 -> opacity = 1, textOpacity = 0
-  //     // scrollPos = 310 -> opacity = 1, textOpacity = 1
-  //     const offset = 300;
-  //     const textOffset = 10;
-  //     if (scrollPos < offset) {
-  //       const newOpacity = 1 - (offset - scrollPos) / offset;
-  //       setOpacity(newOpacity);
-  //       setTextOpacity(0);
-  //     } else {
-  //       setOpacity(1);
-  //       const delta = scrollPos - offset;
-  //       const newTextOpacity = 1 - (textOffset - delta) / textOffset;
-  //       setTextOpacity(newTextOpacity);
-  //     }
-  //   }
 
   useEffect(() => {
     async function f() {
@@ -50,8 +32,19 @@ export default function Favourites({
   }, [session]);
 
   return (
-    <div>
+    <div className=" flex-col items-center justify-center h-screen">
       <Profile />
+      <section
+        className={`flex items-end space-x-7 bg-gradient-to-b from-purple-500 to-purple-1200 h-70 text-white p-8`}
+      >
+        <HeartIcon className="w-12 h-12" />
+
+        <div>
+          <h1 className="text-2xl md:text-3xl lg:text-5xl font-extrabold">
+            Liked Songs
+          </h1>
+        </div>
+      </section>
       <div className="relative-top-20 h-screen overflow-y-scroll w-screen bg-neutral-900">
         <div className="text-white px-8 flex flex-col space-y-1 pb-28">
           {favourites?.map((track, i) => (
