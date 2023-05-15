2fd16ae04a43a493bd3046bce0eac7d1bfbc8525
samarth-shinde <samarthshinde247@gmail.com>
2023-05-15 17:59:20 +0530

    added next and previous button functions

--- components/Player.jsx
+++ components/Player.jsx
@@ -1,4 +1,5 @@
 import { PauseCircleIcon, PlayCircleIcon } from "@heroicons/react/24/solid";
+import { ForwardIcon, BackwardIcon } from "@heroicons/react/24/outline";
 import { useSession } from "next-auth/react";
 import React, { useEffect, useState } from "react";
 
@@ -10,6 +11,22 @@ const Player = ({
 }) => {
   const { data: session } = useSession();
   const [songInfo, setSongInfo] = useState(null);
+
+  async function getDeviceId() {
+    if (session && session.accessToken) {
+      const response = await fetch(
+        `https://api.spotify.com/v1/me/player/devices`,
+        {
+          headers: {
+            Authorization: `Bearer ${session.accessToken}`
+          }
+        }
+      );
+      const data = await response.json();
+      const deviceID = data.devices[0].id;
+      return deviceID;
+    }
+  }
 
   async function fetchSongInfo(trackId) {
     if (trackId) {
@@ -41,6 +58,42 @@ const Player = ({
     }
     const data = await response.json();
     return data;
+  }
+
+  async function handleSkip(e) {
+    const deviceID = await getDeviceId();
+
+    if (e === "forward") {
+      const response = await fetch(
+        `https://api.spotify.com/v1/me/player/next?device_id=${deviceID}`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${session.accessToken}`
+          }
+        }
+      );
+
+      console.log("on Next", response.status);
+      const data = await getCurrentlyPlaying();
+      setGlobalCurrentSongId(data.item.id);
+      setGlobalIsTrackPlaying(true);
+    } else {
+      const response = await fetch(
+        `https://api.spotify.com/v1/me/player/previous?device_id=${deviceID}`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${session.accessToken}`
+          }
+        }
+      );
+
+      console.log("on previous", response.status);
+      const data = await getCurrentlyPlaying();
+      setGlobalCurrentSongId(data.item.id);
+      setGlobalIsTrackPlaying(true);
+    }
   }
 
   async function handlePlayPause() {
@@ -115,11 +168,21 @@ const Player = ({
         </div>
       </div>
       <div className="flex items-center justify-center">
+        <BackwardIcon
+          onClick={async () => await handleSkip("backward")}
+          name="backword"
+          className="h-6 w-6 transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+        />
         {globalIsTrackPlaying ? (
           <PauseCircleIcon onClick={handlePlayPause} className="h-10 w-10" />
         ) : (
           <PlayCircleIcon onClick={handlePlayPause} className="h-10 w-10" />
         )}
+        <ForwardIcon
+          onClick={async () => await handleSkip("forward")}
+          className="h-6 w-6 "
+          name="forward"
+        />
       </div>
       <div></div>
     </div>
